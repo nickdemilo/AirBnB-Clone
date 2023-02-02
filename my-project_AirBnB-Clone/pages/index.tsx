758f7eebe913b66db5539c837ffb4412875bc7ec
nickdemilo <nick880120@gmail.com>
2023-02-02 17:56:47 -0500

    New componets

--- my-project_AirBnB-Clone/pages/index.tsx
+++ my-project_AirBnB-Clone/pages/index.tsx
@@ -1,20 +1,77 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
-import Banner from '../components/Banner'
+import type { NextPage } from "next"
+import Head from "next/head"
+import Header from "../components/Header"
+import Banner from "../components/Banner"
+import SmallCard from "../components/SmallCard"
+import MediumCard from "../components/MediumCard"
+import LargeCard from "../components/LargeCard"
+import Footer from "../components/Footer"
 
-const Home: NextPage = () => {
+ export default function Home({ exploreData, cardsData }) {
   return (
     <div>
       <Head>
-        <title>AirBnB-Clone</title>
+        <title>AirBnB-Clone</title> 
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
 <Header />
 <Banner />
-    </div>
-    )
+
+  <main className="max-w-7xl mx-auto px-8 sm:px-16">
+    <section className="pt-6">
+      <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+      <div className="grid grid-color-1 sm:grid-cols-2 lg:grid-cols-3 lx:grid-cols-4">
+      {exploreData?.map(({img, distance, location}) => (
+        <SmallCard
+          key={img}
+          img={img}
+          distance={distance}
+          location={location}
+          />
+      ))}
+      </div>
+    </section>
+    
+    <section>
+        <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+        <div className="flex space-x-3 overflaw-scroll scrollbar-hide p-3 -ml-3">
+        {cardsData?.map(({img, title}) => (
+          <MediumCard key={img} img={img} title={title}/>
+        ))}
+        </div>
+    </section>
+
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest outdoors"
+            description="Wishlists curated by Airbnb."
+            buttonText="get Inspired"
+          />
+
+    </main>
+
+    <Footer />
+  </div>
+    );
 }
 
-export default Home
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").
+  then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    }
+  }
+}
